8b3a776cecb6c8606e9166e27277b6232a733b8e
Mohammad_shahzaib07 <shahzaib@quolam.com>
2023-11-10 20:26:54 +0530

    integrated form api

--- src/pages/home/contact-form.jsx
+++ src/pages/home/contact-form.jsx
@@ -2,14 +2,62 @@ import Container from "../../components/container";
 import Behance from "../../assets/behance.png";
 import Instagram from "../../assets/instagram.png";
 import Linkedin from "../../assets/linkedin.png";
+import { useState } from "react";
 
 const INSTAGRAM = "https://instagram.com/tabishvisuals?igshid=MzMyNGUyNmU2YQ==";
 const BEHANCE = "https://www.behance.net/khantabish";
 const LINKEDIN = "https://instagram.com/tabishvisuals?igshid=MzMyNGUyNmU2YQ==";
 
 function ContactForm() {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    message: "",
+    accessKey: "90589f42-9f8a-48d4-aa3c-b8ad21af5f46",
+  });
+  const [busy, setBusy] = useState(null);
+
+  const onInputChange = (e) => {
+    const key = e.target.name;
+    const value = e.target.value;
+    setState((prevState) => ({ ...prevState, [key]: value }));
+  };
+
   const socialMediaHandler = (url) => {
     window.open(url, "_blank");
+  };
+
+  const formSubmitHandler = async (e) => {
+    e.preventDefault();
+    setBusy("loading");
+    try {
+      const response = await fetch("https://api.staticforms.xyz/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(state),
+      });
+
+      if (response.ok) {
+        setBusy(null);
+        alert(
+          "Form submitted successfully! ✅, I will get back to you soon 😊"
+        );
+        setState((prevState) => ({
+          ...prevState,
+          name: "",
+          email: "",
+          message: "",
+        }));
+      } else {
+        setBusy("error");
+        alert("Something went wrong! 🤯, Please try again!");
+      }
+    } catch (error) {
+      setBusy("error");
+      alert("Something went wrong! 🤯, Please try again!");
+    }
   };
 
   return (
@@ -50,16 +98,20 @@ function ContactForm() {
               Get in touch
             </h2>
 
-            <form
-              action=""
-              onSubmit={(e) => e.preventDefault()}
-              className="flex flex-col gap-4"
-            >
+            <form className="flex flex-col gap-4" onSubmit={formSubmitHandler}>
+              {/* <input
+                type="hidden"
+                name="accessKey"
+                value="90589f42-9f8a-48d4-aa3c-b8ad21af5f46"
+              /> */}
+
               <input
                 required
                 name="name"
                 placeholder="Name"
                 className="py-3.5 px-4 w-full rounded-lg outline-none placeholder:text-[#322F35] placeholder:font-inter"
+                onChange={onInputChange}
+                value={state.name}
               />
               <input
                 required
@@ -67,6 +119,8 @@ function ContactForm() {
                 type="email"
                 placeholder="Email"
                 className="py-3.5 px-4 w-full rounded-lg outline-none placeholder:text-[#322F35] placeholder:font-inter"
+                onChange={onInputChange}
+                value={state.email}
               />
               <textarea
                 required
@@ -74,13 +128,15 @@ function ContactForm() {
                 placeholder="Message"
                 rows={4}
                 className="py-3.5 px-4 w-full rounded-lg outline-none placeholder:text-[#322F35] placeholder:font-inter"
+                onChange={onInputChange}
+                value={state.message}
               />
 
               <button
                 type="submit"
                 className="w-full h-12 lg:h-14 mt-2 lg:mt-4 rounded-2xl bg-secondary text-[#fff] text-xl lg:text-2xl lg:leading-[3rem] font-medium font-inter border-4 border-[#000] box-border"
               >
-                Contact Me
+                {busy === "loading" ? "Loading..." : "Contact Me"}
               </button>
             </form>
           </div>
