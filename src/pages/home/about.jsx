d65b86b85369722fca77426fb57d72b19eca579b
Mohammad_shahzaib07 <shahzaib@quolam.com>
2023-11-10 20:34:41 +0530

    updated about me

--- src/pages/home/about.jsx
+++ src/pages/home/about.jsx
@@ -23,6 +23,24 @@ function About() {
             experts, honing my skills and gaining invaluable insights into the
             art of creating seamless digital experiences.
           </p>
+
+          <h2 className="font-grotesque text-2xl lg:text-[2rem] lg:leading-[3.575rem] text-secondary mb-2 md:mb-4">
+            My Journey and Expertise
+          </h2>
+          <p className="font-inter text-base lg:text-lg text-[#3A3D40] mb-2 md:mb-4">
+            My journey in the realm of design started with my fascination for
+            photography and editing, where I developed a keen eye for aesthetics
+            and visual storytelling. This passion naturally led me to explore
+            the captivating world of user interface and user experience design,
+            where I discovered my true calling.
+          </p>
+          <p className="font-inter text-base lg:text-lg text-[#3A3D40] ">
+            Throughout my UI/UX learning journey, I&apos;ve embraced the latest
+            tools and technologies, and today, I&apos;m proficient in utilizing
+            Figma to bring my creative visions to life. Figma has become my
+            trusted canvas, allowing me to translate ideas into visually
+            stunning and user-friendly designs.
+          </p>
         </div>
         <div className="flex-1 flex justify-start lg:justify-end">
           <img src={Tabish} alt="tabish profile pic" className="lg:h-[42rem]" />
